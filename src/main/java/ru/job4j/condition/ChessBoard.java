653e9b7c7e498bb85cbd84e4524ea1ee687f08c4
Roman Koshelev <termopsis@mail.ru>
2022-12-24 01:17:49 +0300

    4.3.6. Math.abs [#373616 #374173] remove nested "IF" construction.

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -4,25 +4,27 @@ public class ChessBoard {
 
     public static int wayRook(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if (isValid(x1) && isValid(x2)
-                && isValid(y1) && isValid(y2)) {
-            if (x1 == x2 || y1 == y2) {
-                rsl = Math.abs(x2 - x1);
-                rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
-            }
+        if (!(isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2))) {
+            return rsl;
+        }
+        if (x1 == x2 || y1 == y2) {
+            rsl = Math.abs(x2 - x1);
+            rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
         }
         return rsl;
     }
 
     public static int wayBishop(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if (isValid(x1) && isValid(x2)
-                && isValid(y1) && isValid(y2)) {
-            if (x1 != x2 && y1 != y2
-                    && (Math.abs(x1 - y1) == Math.abs(x2 - y2)
-                    || Math.abs(x1 + y1) == Math.abs(x2 + y2))) {
-                rsl = Math.abs(x2 - x1);
-            }
+        if (!(isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2))) {
+            return rsl;
+        }
+        if (x1 != x2 && y1 != y2
+                && (Math.abs(x1 - y1) == Math.abs(x2 - y2)
+                || Math.abs(x1 + y1) == Math.abs(x2 + y2))) {
+            rsl = Math.abs(x2 - x1);
         }
         return rsl;
     }
