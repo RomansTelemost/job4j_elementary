2efe3d97c4fe22ad399e78f94877d17396454800
Roman Koshelev <termopsis@mail.ru>
2022-12-24 11:26:12 +0300

    2. Тесты для задачи "идеальный вес" [#159458 #374077] refactor

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -3,15 +3,13 @@ package ru.job4j.calculator;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import static ru.job4j.calculator.Fit.*;
-
 class FitTest {
 
     @Test
     void whenManHeight181ThenManWeight93dot14() {
         short manHeight = 181;
         double expected = 93.14;
-        double actual = manWeight(manHeight);
+        double actual = Fit.manWeight(manHeight);
         double delta = 0.01d;
         Assert.assertEquals(expected, actual, delta);
     }
@@ -20,7 +18,7 @@ class FitTest {
     void whenWomanHeight181ThenWomanWeight81dot64() {
         short womanHeight = 181;
         double expected = 81.64;
-        double actual = womanWeight(womanHeight);
+        double actual = Fit.womanWeight(womanHeight);
         double delta = 0.01d;
         Assert.assertEquals(expected, actual, delta);
     }
