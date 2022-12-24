b49182eca01908fe1e7349024acb822774f956b3
Roman Koshelev <termopsis@mail.ru>
2022-12-24 11:19:45 +0300

    4.3.6. Math.abs [#373616 #374173] fix

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -17,14 +17,11 @@ public class ChessBoard {
 
     public static int wayBishop(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if (!(isValid(x1) && isValid(x2)
-                && isValid(y1) && isValid(y2))) {
-            return rsl;
-        }
-        if (x1 != x2 && y1 != y2
-                && (Math.abs(x1 - y1) == Math.abs(x2 - y2)
-                || Math.abs(x1 + y1) == Math.abs(x2 + y2))) {
-            rsl = Math.abs(x2 - x1);
+        if (isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2)) {
+            if (Math.abs(x1 - y1) == Math.abs(x2 - y2)) {
+                rsl = Math.abs(x2 - x1);
+            }
         }
         return rsl;
     }
