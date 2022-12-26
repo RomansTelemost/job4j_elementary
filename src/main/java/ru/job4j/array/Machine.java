8220cf2a65adfd011a67d29ee927539a7800f7b1
Roman Koshelev <termopsis@mail.ru>
2022-12-27 00:26:33 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741 #374931] fix

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -9,13 +9,11 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int reminder = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (reminder - coins[i] >= 0) {
-                if (reminder - coins[i] >= 0) {
-                    reminder -= coins[i];
-                    rsl[size] = coins[i];
-                    size++;
-                }
+        for (int coin : coins) {
+            while (reminder >= coin) {
+                reminder -= coin;
+                rsl[size] = coin;
+                size++;
             }
         }
         return Arrays.copyOf(rsl, size);
