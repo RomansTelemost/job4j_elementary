f1d76edd9e5efa97e2439f02dc69278a2619f462
Roman <termopsis@mail.ru>
2024-07-18 21:40:02 +0300

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,22 +2,26 @@ package ru.job4j.calculator;
 
 public class Fit {
 
+    private static final int MAN_HEIGHT_OFFSET = 100;
+    private static final int WOMAN_HEIGHT_OFFSET = 110;
+
+    private static final String FORMAT_OUTPUT_MESSAGE = "Ideal weight for a %s with height %s cm is %s";
+
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        return rsl;
+        return (height - MAN_HEIGHT_OFFSET) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return (height - WOMAN_HEIGHT_OFFSET) * 1.15;
     }
 
     public static void main(String[] args) {
         short height = 187;
-        double man = Fit.manWeight(height);
-        System.out.println("Man 187 is " + man);
 
-        double woman = womanWeight(height);
-        System.out.println("Woman " + height + " is " + woman);
+        double manWeight = manWeight(height);
+        double womanWeight = womanWeight(height);
+
+        System.out.println(String.format(FORMAT_OUTPUT_MESSAGE, "man", height, String.format("%.2f", manWeight)));
+        System.out.println(String.format(FORMAT_OUTPUT_MESSAGE, "woman", height, String.format("%.2f", womanWeight)));
     }
 }
