2579ea5dddfb2492740151c2e803ee293d3c5e1f
Roman <termopsis@mail.ru>
2024-07-20 14:48:05 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049] - task - refactor

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -4,23 +4,30 @@ public class SimpleStringEncoder {
 
     public static String encode(String inputString) {
         StringBuilder result = new StringBuilder();
+        char symbol = inputString.charAt(0);
         int symbolCounter = 1;
-        for (int i = 0; i <= inputString.length() - 1; i++) {
-            if (i == inputString.length() - 1 || inputString.charAt(i) != inputString.charAt(i + 1)) {
-                result.append(inputString.charAt(i));
-                if (symbolCounter != 1) {
-                    result.append(symbolCounter);
-                    symbolCounter = 1;
-                }
-            } else {
+
+        for (int i = 1; i < inputString.length(); i++) {
+            if (inputString.charAt(i) == symbol) {
                 symbolCounter++;
+                continue;
             }
+            result.append(symbol);
+            symbol = inputString.charAt(i);
+            if (symbolCounter != 1) {
+                result.append(symbolCounter);
+                symbolCounter = 1;
+            }
+        }
+        result.append(symbol);
+        if (symbolCounter != 1) {
+            result.append(symbolCounter);
         }
         return result.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(encode("aaabbc"));
+        System.out.println(encode("aaabbbc"));
 
     }
 }
